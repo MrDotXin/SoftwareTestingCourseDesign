4acd650cb3c198edc4fa4080ca5840e091d447b8
mrdotxin <3099856626@qq.com>
2025-06-03 09:13:23 +0800

    。

---
+++ PropSmart/backend/src/main/java/com/mrdotxin/propsmart/PropSmartApplication.java
@@ -0,0 +1,13 @@
+package com.mrdotxin.propsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropSmartApplication.class, args);
+    }
+
+}
---
+++ PropSmart/backend/src/test/java/com/mrdotxin/propsmart/PropSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdotxin.propsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
